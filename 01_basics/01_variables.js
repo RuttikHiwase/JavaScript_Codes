6778cecaa2dd65b8bb6bb689729c41cdf7be766d
RuttikHiwase <89509037+RuttikHiwase@users.noreply.github.com>
2024-12-07 18:26:43 +0000

    Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=11332
+let accountEmail="rutti32@gmail.com"
+var accountpass="12345"
+accountCity="jaipur"
+
+//accountId=2 // Not Allowed 
+accountEmail="rutt122gmail"
+accountpass="5432"
+accountCity="nagpur"
+let accountstate //undefined
+
+
+// Console Table
+console.table([accountId,accountEmail,accountpass,accountCity,accountstate])
+
+/*
+Note: 
+--Prefer not to use var,because of the issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs,not browser
+
+//code readability should be high
+
+
+let name ="ruttik"
+let age=24
+let isLogged =false
+
+
+    // Primitive Datatypes
+//number =>2 to power 53
+//bigint
+// string => ""
+//boolean => true/false
+//null => representation of empty value , typeOf of null is object
+//undefined => means varible declared but value not given. TypeOf of undefined is undefined only.
+// symbol => to find uniqueness in components
+
+
+// Non-Primitive datatypes
+
+//object
