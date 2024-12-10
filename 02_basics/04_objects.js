de66f7b629c293e5f8b4b8455e83e44cb91e69ce
RuttikHiwase <89509037+RuttikHiwase@users.noreply.github.com>
2024-12-10 13:13:07 +0000

    Done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,8 +28,33 @@ const obj3=Object.assign(obj1,obj2)
 //const obj3=Object.assign({},obj1,obj2)----{} is good to use
 console.log(obj3);
 
-const obj4={...obj1,...obj2}
+const obj4={...obj1,...obj2}//mostly we use spread operator instead of assign()
 console.log(obj4);
+
+const users=[
+    {
+        id:1,
+        email:"h@gmail.com",
+    },
+    {
+        id:2,
+        email:"R@gmail.com",
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
 
 
 
